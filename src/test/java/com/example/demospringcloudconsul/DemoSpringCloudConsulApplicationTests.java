e9e57e57b3b2073982b891f4b78409a5eaec59b1
Olga Maciaszek-Sharma <olga.maciaszek@gmail.com>
2022-02-10 12:33:55 +0100

    Init demo.

---
+++ src/test/java/com/example/demospringcloudconsul/DemoSpringCloudConsulApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.demospringcloudconsul;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringCloudConsulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
